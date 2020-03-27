53ade80a3087f546cd39c88f5ab3e04905f7c63c
sai <sai@gmail.com>
2020-03-27 19:10:52 +0700

    init

---
+++ golang/golangbot_com/structs5.go
@@ -0,0 +1,17 @@
+
+package main
+
+import "fmt"
+
+type Employee struct {
+    firstName, lastName string
+    age, salary         int
+}
+
+func main() {
+    emp6 := Employee{"Sam", "Anderson", 55, 6000}
+    fmt.Println("First Name:", emp6.firstName)
+    fmt.Println("Last Name:", emp6.lastName)
+    fmt.Println("Age:", emp6.age)
+    fmt.Println("Salary:", emp6.salary)
+}
