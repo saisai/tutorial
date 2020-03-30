31d57e22d5052c2bf5586aca9a37430f2ac9c5b2
sai <sai@gmail.com>
2020-03-30 12:55:25 +0700

    init

---
+++ golang/golangbot_com/Concurrency/004.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+    var a chan int
+    if a == nil {
+
+        fmt.Println("Channel a is nil, going to defien it")
+        a = make(chan int)
+        fmt.Printf("Type of a is %T", a)
+    }
+}
