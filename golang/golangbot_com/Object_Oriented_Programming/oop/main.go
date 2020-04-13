83a0bdafc2eb290fa4bbba3fe3de9e047277e4bb
sai <sai@gmail.com>
2020-04-13 12:05:53 +0700

    init

---
+++ golang/golangbot_com/Object_Oriented_Programming/oop/main.go
@@ -0,0 +1,16 @@
+
+package main
+
+import "oop/employee"
+
+func main() {
+
+    e := employee.Employee {
+        FirstName: "Sam",
+        LastName:   "Adolf",
+        TotalLeaves:    30,
+        LeavesTaken:    20,
+    }
+
+    e.LeavesRemaining()
+}
---
+++ python/python-cookbook/11/creating_a_tcp_server/echoserv.py
@@ -0,0 +1,18 @@
+from socketserver import BaseRequestHandler, TCPServer
+
+class EchoHandler(BaseRequestHandler):
+
+    def handle(self):
+        print("Gog connection from", self.client_address)
+        while True:
+            
+            msg = self.request.recv(8192)
+            print(len(msg))
+            if not msg:
+                break
+            self.request.send(msg)
+
+if __name__ == "__main__":
+    serv = TCPServer(("", 20000), EchoHandler)
+    print("Echo server running on port 20000")
+    serv.serve_forever()
